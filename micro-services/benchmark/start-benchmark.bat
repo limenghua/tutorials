721acea6a1e170f54efb672b0eeaf72f00d34dc4
576566385@qq.com <576566385@qq.com>
2017-08-27 22:24:29 +0800

    add apache ab.exe

--- micro-services/benchmark/start-benchmark.bat
+++ micro-services/benchmark/start-benchmark.bat
@@ -1,7 +1,7 @@
 start node.exe index.js
-ab -n 1000 -c 100  http://127.0.0.1:8000/
-ab -n 1000 -c 100  http://127.0.0.1:8080/
-ab -n 1000 -c 100  http://127.0.0.1:9000/
-ab -n 1000 -c 100  http://127.0.0.1:8000/hello/
-ab -n 1000 -c 100  http://127.0.0.1:8080/hello/
-ab -n 1000 -c 100  http://127.0.0.1:9000/hello/
+bin\ab -n 1000 -c 100  http://127.0.0.1:8000/
+bin\ab -n 1000 -c 100  http://127.0.0.1:8080/
+bin\ab -n 1000 -c 100  http://127.0.0.1:9000/
+bin\ab -n 1000 -c 100  http://127.0.0.1:8000/hello/
+bin\ab -n 1000 -c 100  http://127.0.0.1:8080/hello/
+bin\ab -n 1000 -c 100  http://127.0.0.1:9000/hello/
